45169c04d55ac3e3202c63bb57349e621481cee9
willalwin <35215476+willalwin@users.noreply.github.com>
2020-02-08 15:52:08 -0800

    Add files via upload

---
+++ sys1.sql
@@ -0,0 +1,37 @@
+--current location
+Show con_name;
+
+--show db names
+Select name, con_id 
+from v$pdbs;
+
+--alter session to pluggable db
+Alter session set container=orclpdb;
+
+--show new location
+Show con_name;
+
+--current db and read write info
+select name, open_mode from v$pdbs;
+
+--open db
+alter PLUGGABLE DATABASE open;
+
+--check current user list
+select * from all_users;
+
+--create capstone user
+create user capstone identified by captsone;
+
+--grant privileges to capstone user
+grant create session to capstone;
+
+grant create table to capstone;
+
+grant unlimited tablespace to capstone;
+
+grant create sequence to capstone;
+
+grant create view to capstone;
+
+grant create synonym to capstone;
